6dcca19423530d2adfdbc1c436239f7ff40171f9
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-21 19:59:47 -0400

    Create IMolMinimal.sol
    add molochian interface 👹

---
+++ mol/v2/interfaces/IMolMinimal.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity 0.6.12;
+
+/// @notice Brief interface for Moloch DAO v2. 
+interface IMolMinimal { 
+    function submitProposal(
+        address applicant,
+        uint256 sharesRequested,
+        uint256 lootRequested,
+        uint256 tributeOffered,
+        address tributeToken,
+        uint256 paymentRequested,
+        address paymentToken,
+        string calldata details
+    ) external returns (uint256);
+    
+    function getProposalFlags(uint256 proposalId) external view returns (bool[6] memory);
+    
+    function withdrawBalance(address token, uint256 amount) external;
+}
