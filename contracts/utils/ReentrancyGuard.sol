9b7b3b79d86c1d05c3c10aab5e9c6122c40aa56a
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-21 20:29:22 -0400

    Create ReentrancyGuard.sol
    👷 add reentrancy guard utility

---
+++ contracts/utils/ReentrancyGuard.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.6.12 <0.9.0;
+
+/// @notice Contract module that helps prevent reentrant calls to a function.
+abstract contract ReentrancyGuard {
+    uint256 status = 1;
+    
+    modifier nonReentrant() {
+        require(status == 1, "reentrant"); 
+        status = 2; 
+        _;
+        status = 1;
+    }
+}
