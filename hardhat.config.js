3b380ff3193b56ef99b2cae70ad86ae3bd19003f
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-23 18:32:28 -0400

    Update hardhat.config.js
    nit

--- hardhat.config.js
+++ hardhat.config.js
@@ -62,7 +62,7 @@ module.exports = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 999999,
+            runs: 200,
           },
         },
       },
@@ -71,7 +71,7 @@ module.exports = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 999999,
+            runs: 200,
           },
         },
       },
